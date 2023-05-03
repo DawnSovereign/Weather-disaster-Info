bdd787353398c4be81c017c0ace2863c6d10913d
DawnyCinders <jasokim@csumb.edu>
2023-05-03 15:22:20 -0700

    hey changed this

--- AirQuality.py
+++ AirQuality.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-import requests, json
+import requests
+
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
@@ -8,19 +9,20 @@ bootstrap5 = Bootstrap5(app)
 api_key = '6fc459c195d2f30852cf3074447bdeafa7a0eb202abc1fbb18a9b4ba882d5b1f'
 
 payload = {
-  'api_key': api_key,
-  'start_date': '2020-11-05',
-  'end_date': '2020-11-08'
+  'api_key': api_key
 }
 
 endpoint = 'https://api-dashboard.getambee.com/#/'
 
 @app.route('/')
 def main():
+    data = None
     try:
+        print('ran')
         r = requests.get(endpoint, params=payload)
         data = r.json()
+        # print(data)
         print(data)
-    except:
-        print('please try again')
-    return render_template('Air.html', data=data)
+    except Exception as e:
+        print('please try again', e)
+    return render_template('Air.html', data=data) 
