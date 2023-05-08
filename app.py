f73435bff532e51263a374570185e761e077b892
dylyou <dyluri007@gmail.com>
2023-05-07 18:27:26 -0700

    Finished implementation of current weather forecast searching

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
-
+from forecast import ZipcodeForm, getForecastData
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+app.config['SECRET_KEY'] = 'csumb-otter'
+
+current_weather_api = '7c66fb6dc5b98966d3c9709307417f82'
+forecastinfo = None
 
 @app.route('/')
 def home():
@@ -12,12 +16,39 @@ def home():
 def tornado():
     return render_template("tornado.html")
 
+@app.route('/forecast', methods = ['GET', 'POST'])
+def forecast():
+    form = ZipcodeForm()
+    global forecastinfo
+    if form.validate_on_submit():
+        print('Form validated as submited')
+        try:
+            forecastinfo = getForecastData(form.user_zip.data)
+            print('About to attempt redirect')
+            return redirect('/forecast_view')
+        except Exception as e:
+            print("Error on running function: ", e)
+            return redirect('/') 
+    return render_template('forecastSearch.html', form = form)
+
+
+@app.route('/forecast_view', methods = ['GET', 'POST'])
+def forecastview():
+    if forecastinfo == None:
+        return redirect('/')
+    else: 
+        return render_template('forecastView.html', forecastinfo = forecastinfo)
+    # return render_template('forecastView.html', forecastinfo = forecastinfo)
+    
+
 @app.route('/handle_selection', methods=['POST'])
 def handle_selection():
     selected_option = request.form.get('selected_option')
 
     if selected_option == 'tornado':
         return redirect(url_for('tornado'))
+    if selected_option == 'weather_forecast':
+        return redirect(url_for('forecast'))
 
     return redirect(url_for('home'))  # Home page is redirected if no option is selected 
 
---
+++ forecast.py
@@ -0,0 +1,40 @@
+
+import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from pprint import pprint
+forecast_api_key = '7c66fb6dc5b98966d3c9709307417f82'
+class ZipcodeForm(FlaskForm):
+        user_zip = StringField(
+            'Enter your ZipCode:', 
+            validators= [DataRequired()]
+        )
+   
+
+def getForecastData(userinput):
+    """This function will take in user
+    input, which should be a zipcode from somewhere in the
+    US. This function will then return a dictionary of weather
+    information, or return None if a error occurs"""
+    location_link = f'http://api.openweathermap.org/geo/1.0/zip?zip={userinput}&appid={forecast_api_key}'
+    try:
+        r = requests.get(location_link)
+        local = r.json()
+        lat = local['lat']
+        lon = local['lon']
+        forecast_link = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={forecast_api_key}&units=imperial'
+    except Exception as e:
+        print("Error on zipcode:", e) 
+    try:
+        r = requests.get(forecast_link)
+        data = r.json()
+        return data
+    except Exception as e:
+        print("Error on forecast: ", e)
+    return None
+
+
+# ans = input("Enter a zipcode ")
+# data = getForecastData(ans)
+# pprint(data)
