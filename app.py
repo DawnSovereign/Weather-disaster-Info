4dbc8f5c9e282a19fbaaf3c1525cb588c55ab93e
alopezCSUMB <alopezmartinez@csumb.edu>
2023-05-08 16:12:12 -0700

    added API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
-
+import requests, json
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
@@ -12,14 +12,39 @@ def home():
 def tornado():
     return render_template("tornado.html")
 
-@app.route('/handle_selection', methods=['POST'])
+@app.route('/earthquakes', methods=['GET'])
+def earthquakes():
+    start_date = '2023-02-01'
+    end_date = '2023-02-28'
+    url = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}'
+    
+    response = requests.get(url)
+    data = response.json()
+    # print(data)
+
+    earthquakes = []
+    features = data.get('features', [])
+    for feature in features:
+        place = feature['properties']['place']
+        magnitude = feature['properties']['mag']
+        earthquake = {'place': place, 'magnitude': magnitude}
+        earthquakes.append(earthquake)
+
+    return render_template('earthquakes.html', earthquakes=earthquakes, data=data)
+
+
+ 
+
+@app.route('/handle_selection', methods = ['GET', 'POST'])
 def handle_selection():
     selected_option = request.form.get('selected_option')
 
     if selected_option == 'tornado':
         return redirect(url_for('tornado'))
+    
+    elif selected_option == 'earthquakes':
+        return redirect(url_for('earthquakes'))
 
     return redirect(url_for('home'))  # Home page is redirected if no option is selected 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
--- templates/earthquake.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template
-import requests, json
-from flask_bootstrap import Bootstrap5
-
-app = Flask(__name__)
-bootstrap5 = Bootstrap5(app)
