7c7926b46d52084bfe1a4e745764b29b63277705
alopezCSUMB <alopezmartinez@csumb.edu>
2023-05-08 22:28:42 -0700

    Added css to home page and earthquake page

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_bootstrap import Bootstrap5
+import urllib.request, json
+import json
+import os
 import requests
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
 
 @app.route('/earthquakes', methods=['GET'])
 def earthquakes():
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
-    url = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}&minmagnitude=5'
+    start_date = '2023-05-01'
+    end_date = '2023-05-08'
+    magnitude = '5'
+    # start_date = request.args.get('start_date')
+    # end_date = request.args.get('end_date')
+    # magnitude = request.args.get('magnitude')
+    url = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}&minmagnitude={magnitude}'
+
 
     response = requests.get(url)
     data = response.json()
+    print(data)
 
     earthquakes = []
     features = data.get('features', [])
@@ -23,63 +41,15 @@ def earthquakes():
     cities = [earthquake['place'] for earthquake in earthquakes]
     magnitudes = [earthquake['magnitude'] for earthquake in earthquakes]
 
-    return render_template('earthquakes.html', cities=cities, magnitudes=magnitudes)
+    return render_template('earthquakes.html', cities=cities, magnitudes=magnitudes, data=data)
 
-
-
-
-
-# from flask import Flask, render_template, request, redirect, url_for
-# import requests
-# from flask_bootstrap import Bootstrap5
-
-
-# app = Flask(__name__)
-# bootstrap = Bootstrap5(app)
-
-# @app.route('/')
-# def home():
-#     return render_template("home.html")
-
-# @app.route('/tornado')
-# def tornado():
-#     return render_template("tornado.html")
-
-# @app.route('/earthquakes', methods=['GET'])
-# def earthquakes():
-#     start_date = request.args.get('start_date')
-#     end_date = request.args.get('end_date')
-#     magnitude = request.args.get('magnitude')
+@app.route('/handle_selection', methods = ['GET', 'POST'])
+def handle_selection():
+    selected_option = request.form.get('selected_option')
     
-#     url = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}&minmagnitude={magnitude}'
-#     response = requests.get(url)
-#     data = response.json()
-#     print(data)
-    
-#     earthquakes = []
-#     features = data.get('features', [])
-#     for feature in features:
-#         place = feature['properties']['place']
-#         magnitude = feature['properties']['mag']
-#         earthquake = {'place': place, 'magnitude': magnitude}
-#         earthquakes.append(earthquake)
+    if selected_option == 'earthquakes':
+        return redirect(url_for('earthquakes'))
 
-#     cities = [earthquake['place'] for earthquake in earthquakes]
-#     magnitudes = [earthquake['magnitude'] for earthquake in earthquakes]
-
-#     return render_template('earthquakes.html', cities=cities, magnitudes=magnitudes)
-
-
-# @app.route('/handle_selection', methods = ['GET', 'POST'])
-# def handle_selection():
-#     selected_option = request.form.get('selected_option')
-
-#     if selected_option == 'tornado':
-#         return redirect(url_for('tornado'))
-    
-#     elif selected_option == 'earthquakes':
-#         return redirect(url_for('earthquakes'))
-
-#     return redirect(url_for('home'))  # Home page is redirected if no option is selected 
+    return redirect(url_for('home'))
 
 
