08219bbe4ca6336d69d1aa7fd46e61ec3031d3cb
DawnyCinders <jasokim@csumb.edu>
2023-05-15 16:37:14 -0700

    hey changed this

--- AirQuality.py
+++ AirQuality.py
@@ -1,30 +1,28 @@
 from flask import Flask, render_template
 import requests
-from flask_bootstrap import Bootstrap5
-import os
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
-
-WEATHERBIT_API_KEY = os.environ.get('WEATHERBIT_API_KEY')
-WEATHERBIT_API_URL = 'http://api.weatherbit.io/v2.0/current/airquality'
-
-def get_air_quality_data(city):
-    params = {
-        'city': city,
-        'key': WEATHERBIT_API_KEY
-    }
-    response = requests.get(WEATHERBIT_API_URL, params=params)
-    response.raise_for_status()
-    data = response.json()
-    return data['data'][0]
+bootstrap = Bootstrap(app)
 
 @app.route('/')
-def index():
-    city = 'New York'  # Replace with user input or use a default value
-    try:
-        air_quality_data = get_air_quality_data(city)
-    except requests.exceptions.HTTPError as error:
-        air_quality_data = None
-        print(f"Error: {error}")
-    return render_template('Air.html', data=air_quality_data)
+def home():
+    # Make the API request
+    api_key = '80745c411c034e788af778b4d36e6ffa'
+    latitude = 35.7721
+    longitude = -78.63861
+    url = f"http://api.weatherbit.io/v2.0/current/airquality?key={api_key}&lat={latitude}&lon={longitude}"
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract the required data from the response
+    city = data['city_name']
+    country = data['country_code']
+    aqi = data['data'][0]['aqi']
+    pm25 = data['data'][0]['pm25']
+
+    # Render the template with the data
+    return render_template('Air.html', city=city, country=country, aqi=aqi, pm25=pm25)
+
+if __name__ == '__main__':
+    app.run()
