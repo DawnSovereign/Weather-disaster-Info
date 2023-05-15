8c32bdf47b99fcf99e2cd4d8800444020f158fe6
DawnyCinders <jasokim@csumb.edu>
2023-05-14 22:24:09 -0700

    hey changed this

--- AirQuality.py
+++ AirQuality.py
@@ -1,37 +1,30 @@
 from flask import Flask, render_template
-import requests, _json
+import requests
 from flask_bootstrap import Bootstrap5
+import os
 
 app = Flask(__name__)
-bootstrap5 = Bootstrap5(app)
+bootstrap = Bootstrap5(app)
 
-api_key = '6fc459c195d2f30852cf3074447bdeafa7a0eb202abc1fbb18a9b4ba882d5b1f'
+WEATHERBIT_API_KEY = os.environ.get('WEATHERBIT_API_KEY')
+WEATHERBIT_API_URL = 'http://api.weatherbit.io/v2.0/current/airquality'
 
-payload = {
-  'api_key': api_key
-}
-
-endpoint = 'https://api.ambeedata.com/latest/by-lat-lng?lat=12&lng=77'
-
-
-try:
-    r = requests.get(endpoint, params=payload)
-    local = r.json()
-    ozone = local['OZONE']
-except Exception as e:
-    print('Error: ', e)
-
+def get_air_quality_data(city):
+    params = {
+        'city': city,
+        'key': WEATHERBIT_API_KEY
+    }
+    response = requests.get(WEATHERBIT_API_URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+    return data['data'][0]
 
 @app.route('/')
-def main():
-    
+def index():
+    city = 'New York'  # Replace with user input or use a default value
     try:
-        print('ran')
-        r = requests.get(endpoint, params=payload)
-        data = r.json()
-        
-        print(data)
-
-    except:
-        print('please try again')
-    return render_template('Air.html', data=data) 
+        air_quality_data = get_air_quality_data(city)
+    except requests.exceptions.HTTPError as error:
+        air_quality_data = None
+        print(f"Error: {error}")
+    return render_template('Air.html', data=air_quality_data)
